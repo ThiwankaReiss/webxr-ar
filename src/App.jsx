896ab07a27efece6315d28ddb3febd34d74d6106
ThiwankaReiss <thiwankar2003@gmail.com>
2025-01-14 13:02:35 +0530

    changed routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <Routes>
-      <Route path="/" element={<CubeContainer/>}/>
+      <Route path="/webxr-ar" element={<CubeContainer/>}/>
         
     </Routes>
   )
