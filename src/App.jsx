53a7a53cf08415911ecf01cf471481069613c422
ThiwankaReiss <thiwankar2003@gmail.com>
2025-01-14 13:30:45 +0530

    Added AR support

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import './App.css'
 import CubeContainer from "./componenets/cube/CubeContainer"
 
+import XrCubeContainer from "./componenets/xr-cube/XrCubeContainer"
+
 function App() {
- 
+
   return (
     <Routes>
-      <Route path="/webxr-ar" element={<CubeContainer/>}/>
-        
+      <Route path="/webxr-ar" element={<XrCubeContainer />} />
+      <Route path="/webxr-ar/cube" element={<CubeContainer />} />
+      <Route path="/webxr-ar/xr-cube" element={<XrCubeContainer />} />
     </Routes>
   )
 }
